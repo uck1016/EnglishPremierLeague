29fdd1298e097a2aec7521dac4309d9e69033221
uck1016 <chaitanya.3883@gmail.com>
2015-05-13 00:19:22 -0400

    cleaning

--- Extras/db.js
+++ Extras/db.js
@@ -10,3 +10,12 @@ mongo_factory.getConnection(url).then(function(db){
         //console.log(doc)*/
     })
 module.exports=mongo_factory;
+var AWS=require("aws-sdk");
+//AWS.config.update({accessKeyId: 'AKIAITH3I536I4RXVPKA', secretAccessKey: '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'});
+AWS.config.update({region: 'us-east-1'});
+var s3= new AWS.S3();
+/*s3.getObject({Bucket:config.aws_s3_bucket_name,Key:config.aws_s3_key_name},function(err,res){
+ var body=res.Body;
+ var eplKeySet=(body).toString("utf8");
+ callback(eplKeySet);
+ });*/
